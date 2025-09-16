2082b24129961d3ef756c5d9e36a3f7c8f416602
Jeff <jeffkleinaitis@gmail.com>
2025-09-16 17:17:43 -0400

    refactor: Separate logic into smaller functions

--- main.py
+++ main.py
@@ -13,32 +13,41 @@ def download_images(image_url_list):
             file.write(response.content)
 
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}
-url = "https://www.dignitymemorial.com/obituaries/dallas-tx/richard-cole-7245321"
-request = urllib.request.Request(url, headers=headers)
+def fetch_memory_list(obituary_url):
 
-r = urllib.request.urlopen(request).read()
-html = r.decode('utf-8')
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}
+    request = urllib.request.Request(obituary_url, headers=headers)
 
-soup = BeautifulSoup(html, "html.parser")
+    r = urllib.request.urlopen(request).read()
+    html = r.decode('utf-8')
 
-script_tag = soup.find("script", id="__NEXT_DATA__")
+    soup = BeautifulSoup(html, "html.parser")
 
-parse = json.loads(script_tag.contents[0])
+    script_tag = soup.find("script", id="__NEXT_DATA__")
 
-site_entries = parse["props"]["pageProps"]["memoryData"]["MemoryList"]
+    parse = json.loads(script_tag.contents[0])
 
-url_list = []
+    return parse["props"]["pageProps"]["memoryData"]["MemoryList"]
 
-for entry in site_entries:
-    image_parts = entry["Image"]
-    if image_parts is not None:
-        # Get parts of the URL in reverse order (index 13 -> 0)
-        parts = [image_parts["FullUrl"][i] for i in range(13, -1, -1)]
 
-        # Join parts with '/' to reconstruct the full URL
-        url = "/".join(parts)
+def build_image_urls(site_entries):
 
-        url_list.append(url)
+    url_list = []
 
+    for entry in site_entries:
+        image_parts = entry["Image"]
+        if image_parts is not None:
+            # Get parts of the URL in reverse order (index 13 -> 0)
+            parts = [image_parts["FullUrl"][i] for i in range(13, -1, -1)]
+
+            # Join parts with '/' to reconstruct the full URL
+            url = "/".join(parts)
+
+            url_list.append(url)
+
+    return url_list
+
+
+memory_list = fetch_memory_list("https://www.dignitymemorial.com/obituaries/dallas-tx/richard-cole-7245321")
+url_list = build_image_urls(memory_list)
 download_images(url_list)
